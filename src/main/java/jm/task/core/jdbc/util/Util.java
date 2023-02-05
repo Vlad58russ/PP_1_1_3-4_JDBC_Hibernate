86a19df08477f13724b1546d5e739cde2e2a3e5d
Vlad <vladschenn@yandex.ru>
2023-02-05 11:12:25 +0300

    задача выполненна

--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -1,5 +1,23 @@
 package jm.task.core.jdbc.util;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class Util {
-    // реализуйте настройку соеденения с БД
+    private static final String userName = "root";
+    private static final String password = "root";
+    private static final String url = "jdbc:mysql://localhost:3306/mydbtest";
+    private static final String DB_Driver = "com.mysql.cj.jdbc.Driver";
+    public static Connection getConnection() {
+        Connection connection = null;
+        try {
+            Class.forName(DB_Driver);
+            connection = DriverManager.getConnection(url, userName, password);
+        } catch (ClassNotFoundException | SQLException e) {
+            e.printStackTrace();
+            System.out.println("Connection ERROR");
+        }
+        return connection;
+    }
 }
---
+++ src/test/resources/test.sql
@@ -0,0 +1,5 @@
+#insert into users (name, age, email) values ('Steve', 55, 'steve@devcolibri.com');
+#delete from users;
+#select * from users;
+#select * from users where name = 'Steve';
+#update users set name='Steve', age=55 where id = 1;
